a7a46a360a1867026ce32d9904a88890f3ba836f
zisoo <zisoo_3@naver.com>
2023-04-28 17:21:19 +0900

    feat: [최지수] 게시물 수정 페이지 생성 [FRB-4]

---
+++ Vue/frontend/src/components/ProductModifyForm.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <h3>예비 게시물</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang=""></style>
---
+++ Vue/frontend/src/views/product/ProductModifyPage.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <div>
+        <h2>상품 게시물 수정</h2>
+        <product-modify-form />
+    </div>
+</template>
+
+<script>
+import ProductModifyForm from '@/components/ProductModifyForm.vue';
+export default {
+    components: {
+        ProductModifyForm,
+    },
+}
+</script>
+
+<style lang=""></style>
