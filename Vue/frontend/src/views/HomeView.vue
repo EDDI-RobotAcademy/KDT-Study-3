4822933d2df9d4a60933bc4808f5ef348ab03841
zisoo <zisoo_3@naver.com>
2023-04-30 19:54:00 +0900

    test: [최지수] 불필요한 테스트 코드 삭제 [TESTx]

--- Vue/frontend/src/views/HomeView.vue
+++ Vue/frontend/src/views/HomeView.vue
@@ -2,10 +2,7 @@
   <v-container>
     <div>
       <nav>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> |
-        <router-link to="/product-list-page">상품 게시판</router-link> |
-        <router-link to="/product-modify-page">상품 게시물 수정</router-link> |
+        <router-link to="/product-list-page">상품 게시판</router-link>
       </nav>
     </div>
   </v-container>
