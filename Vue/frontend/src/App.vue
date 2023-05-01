40b1f0343eec7bc972a26fce7619cf63825a0470
iknowca <iknowca@gmail.com>
2023-05-01 20:34:54 +0900

    feat: [김원기] app-bar의 로고 버튼기능 구현 [FRB-6]

--- Vue/frontend/src/App.vue
+++ Vue/frontend/src/App.vue
@@ -6,35 +6,14 @@
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-btn @click="()=>this.$router.push('/')"  color="white">
+          <v-img src="@/assets/logo.svg" max-height="30" max-width="30" contain/>
+        <v-app-bar-title style="color:black">
+          <span>STUDY-2-3</span>
+        </v-app-bar-title>
+        </v-btn>
       </div>
-
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
